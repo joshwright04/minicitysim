a2b0e76c11667784ddb1a9ea5e8841cbea9734b0
Josh Wright <jowr7733@colorado.edu>
2026-04-25 14:36:25 -0600

    House now default to being assigned a random Tenant fix

--- src/main/java/game/BuildingFactory.java
+++ src/main/java/game/BuildingFactory.java
@@ -1,16 +1,20 @@
 package game;
+
+import game.tenant.TenantFactory;
+
 public class BuildingFactory {
 
     public Building createCottage(String name){
         return new House(name, 20, 5);
     }
 
+    TenantFactory tenantFactory = new TenantFactory();
     public Building createHouse(String name){
-        return new House(name, 30, 7);
+        return new House(name, 30, 7, tenantFactory.createRandomTenant());
     }
 
     public Building createMansion(String name){
-        return new House(name, 50, 10);
+        return new House(name, 50, 10, tenantFactory.createRandomTenant());
     }
 
     public Building createFarm(String name){
